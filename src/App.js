f3f33834a0aef2bba9720f9fd534ada29b497b64
Andrey Osipov <mitol5549@gmail.com>
2023-03-13 18:00:57 +0100

    AddUser and Card components added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import AddUser from "./components/Users/AddUser";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
 };
 
 export default App;
