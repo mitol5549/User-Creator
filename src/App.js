4e6f4287554fc2bc2c238fb7136133d0fb3fc11a
Andrey Osipov <mitol5549@gmail.com>
2023-03-15 16:06:55 +0100

    ErrorModal Component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
+import ErrorModal from "./components/UI/ErrorModal";
 
 const App = () => {
   const [usersList, setUsersList] = useState([]);
@@ -15,6 +16,7 @@ const App = () => {
 
   return (
     <div>
+      <ErrorModal title="An error occured!" message="Something went wrong!" />
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
     </div>
