8253cf954f3b663a908e6ee59e935edc144ec2dd
Andrey Osipov <mitol5549@gmail.com>
2023-03-15 15:47:37 +0100

    Managing UsersList added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 const App = () => {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => [
+      ...prevUsersList,
+      { name: userName, age: userAge, key: Math.random().toString() },
+    ]);
+  };
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 };
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -20,7 +20,7 @@ const AddUser = (props) => {
       return;
     }
 
-    console.log(enteredUserName, enteredAge);
+    props.onAddUser(enteredUserName, enteredAge);
     setEnteredUserName("");
     setEnteredAge("");
   };
